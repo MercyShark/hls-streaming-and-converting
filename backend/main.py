a1fd4953fbcd429b6ceda39a83a5dfc8133fb9ae
Rishabh <prajapatirishabh04@gmail.com>
2025-06-29 03:54:57 +0530

    Refactor Dockerfile and main.py for improved S3 integration and presigned URL generation

--- backend/main.py
+++ backend/main.py
@@ -5,14 +5,18 @@ from pathlib import Path
 from motor.motor_asyncio import AsyncIOMotorClient
 from fastapi.staticfiles import StaticFiles
 from bson.objectid import ObjectId
-from datetime import datetime 
+from datetime import datetime
 import uuid
 from fastapi.middleware.cors import CORSMiddleware
 import pika
 import json
 from dotenv import load_dotenv
-import os 
-# Ensure the upload directory exists
+import os
+from pydantic import BaseModel
+import boto3
+from botocore.config import Config
+
+load_dotenv()
 UPLOAD_DIRECTORY = Path("uploads")
 UPLOAD_DIRECTORY.mkdir(exist_ok=True)
 
@@ -20,8 +24,6 @@ HLSDIRECTORY = Path("hls")
 HLSDIRECTORY.mkdir(exist_ok=True)
 
 app = FastAPI()
-
-# channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
 
 app.add_middleware(
     CORSMiddleware,
@@ -31,105 +33,140 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
-app.mount("/media",StaticFiles(directory="uploads"),name="media")
-app.mount("/hls",StaticFiles(directory="hls"),name="hls")
-# MongoDB settings
-# MONGO_DETAILS = "mongodb://localhost:27017" // development
+app.mount("/media", StaticFiles(directory="uploads"), name="media")
+app.mount("/hls", StaticFiles(directory="hls"), name="hls")
+
+ORIGINAL_VIDEO_DIRECTORY_NAME = "original"
+HLS_VIDEO_DIRECTORY_NAME = "hls"
+
 MONGO_DETAILS = os.getenv("MONGO_URL")
 client = AsyncIOMotorClient(MONGO_DETAILS)
 database = client.uploads
 files_collection = database.get_collection("files")
 
+s3_client = boto3.client(
+    "s3",
+    endpoint_url=os.getenv("R2_ENDPOINT_URL"),
+    aws_access_key_id=os.getenv("R2_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("R2_SECRET_KEY"),
+    config=Config(signature_version="s3v4"),
+    region_name="auto",
+)
 
-def get_unique_filename(file_location: Path) -> Path:
-    unique_id = uuid.uuid4().hex
-    new_filename = f"{file_location.stem}_{unique_id}{file_location.suffix}"
-    return file_location.parent / new_filename
 
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile = File(...)):
+
+class GenerateURLRequest(BaseModel):
+    filename: str
+    mime_type: str
+
+@app.post("/generate-presigned-url/")
+def generate_presigned_url(data: GenerateURLRequest):
+    unique_filename = f"{uuid.uuid4()}_{data.filename}"
     try:
-        connection = pika.BlockingConnection(
-            pika.ConnectionParameters( host= os.getenv("RABBITMQ_URL"),heartbeat=0,credentials=pika.PlainCredentials(username=os.getenv("RABBITMQ_DEFAULT_USER"), password=os.getenv("RABBITMQ_DEFAULT_PASS"))))
-        channel = connection.channel()
+        url = s3_client.generate_presigned_url(
+            ClientMethod="put_object",
+            Params={
+                "Bucket": os.getenv("R2_BUCKET_NAME"),
+                "Key": f"{ORIGINAL_VIDEO_DIRECTORY_NAME}/{unique_filename}",
+                "ContentType": data.mime_type
+                },
+            ExpiresIn=os.getenv("PRESIGNED_URL_EXPIRE_TIME") 
+        )
+        return {
+            "presigned_url": url,
+            "path": unique_filename
+        }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+class UploadCompleteRequest(BaseModel):
+    filename: str
+    mime_type: str
+    unique_filename: str
+    size: int | None = None
 
-        channel.queue_declare(queue='video_quality_conversion_queue')
-        file_location : Path = UPLOAD_DIRECTORY / file.filename
-
-        # Check if file already exists and generate a unique filename if it does
-        if file_location.exists():
-            file_location = get_unique_filename(file_location)
-
-        # Save file to disk
-        with open(file_location, "wb") as buffer:
-            shutil.copyfileobj(file.file, buffer)
-        
-        print(file.filename)
-        # Prepare metadata
+@app.post("/upload/complete/")
+async def create_upload_file(data: UploadCompleteRequest):
+    try:
         file_metadata = {
-            "original_filename": file.filename,
-            "unique_filename": file_location.name,
-            "content_type": file.content_type,
-            "file_size": file_location.stat().st_size,
+            "original_filename": data.filename,
+            "unique_filename": data.unique_filename,
+            "content_type": data.mime_type,
+            "file_size": data.size,
+            "is_converted": False,
             "upload_time": datetime.now(),
-            "file_path": str(file_location),
-            "is_converted": "False"
         }
         
-        print(file_metadata)
-        # Generate HLS segments and master playlist
-        print(file_location)
-        print(HLSDIRECTORY / file_location.stem)
-        result = await files_collection.insert_one(file_metadata)
-        body = {
-            "file_path": str(file_location),
-            "output_dir": str(HLSDIRECTORY / file_location.stem),
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(
+                host=os.getenv("RABBITMQ_URL"),
+                heartbeat=0,
+                credentials=pika.PlainCredentials(
+                    username=os.getenv("RABBITMQ_DEFAULT_USER"),
+                    password=os.getenv("RABBITMQ_DEFAULT_PASS"),
+                ),
+            )
+        )
+        channel = connection.channel()
+        channel.queue_declare(queue="video_quality_conversion_queue")
+
+
+        result = await files_collection.insert_one(
+            file_metadata
+        )
+
+
+        queue_body = {
+            "file_path": f"{ORIGINAL_VIDEO_DIRECTORY_NAME}/{data.unique_filename}",
+            "output_dir": f"{HLS_VIDEO_DIRECTORY_NAME}/{data.unique_filename.split('.')[0]}",
             "file_id": str(result.inserted_id),
         }
-        message = json.dumps(body)
-        channel.basic_publish(exchange='', routing_key='video_quality_conversion_queue', body=message)
 
-        # Insert metadata into MongoDB
+        message = json.dumps(queue_body)
+
+        channel.basic_publish(
+            exchange="",
+            routing_key="video_quality_conversion_queue",
+            body=message,
+        )
         channel.close()
         connection.close()
-        return {"file_id": str(result.inserted_id), "unique_filename": file_location.name}
-        # return {"unique_filename": file_location.name}
 
-    except HTTPException as e:
-        raise e
+        return {
+            "file_id": str(result.inserted_id),
+            "unique_filename": data.unique_filename,
+        }
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-# @app.get("/")
-# async def main():
-#     content = """
-#     <body>
-#     <form action="/uploadfile/" enctype="multipart/form-data" method="post">
-#     <input name="file" type="file">
-#     <input type="submit">
-#     </form>
-#     </body>
-#     """
-#     return HTMLResponse(content=content)
+
 
 @app.get("/files/all")
 async def get_all_files():
     files = []
-    instances = files_collection.find({"is_converted" : True},{'_id': 0}).sort("upload_time",-1)
+    instances = files_collection.find({"is_converted": True}, {"_id": 0}).sort(
+        "upload_time", -1
+    )
     async for file in instances:
-        files.append({
-            # "file_id": str(file["_id"]),
-            "original_filename": file["original_filename"],
-            "unique_filename": file["unique_filename"],
-            "content_type": file["content_type"],
-            "file_size": str(file["file_size"]),
-            "upload_time": file["upload_time"].isoformat(),
-            "file_path": file["file_path"],
-            "url" : "http://localhost:8000/media/" + file['unique_filename'],
-            "hls_url" : "http://localhost:8000/hls/" + file['unique_filename'].split('.')[0] + "/master.m3u8"
-        })
+        files.append(
+            {
+                # "file_id": str(file["_id"]),
+                "original_filename": file["original_filename"],
+                "unique_filename": file["unique_filename"],
+                "content_type": file["content_type"],
+                "file_size": str(file["file_size"]),
+                "upload_time": file["upload_time"].isoformat(),
+                "file_path": file["file_path"],
+                "url": "http://localhost:8000/media/" + file["unique_filename"],
+                "hls_url": "http://localhost:8000/hls/"
+                + file["unique_filename"].split(".")[0]
+                + "/master.m3u8",
+            }
+        )
     return files
+
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
